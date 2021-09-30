cd46b409ffd1d72142bc53c42828fe726cee4cbf
chrisjesss <ccalewis@hotmail.com>
2021-09-30 12:20:32 +0200

    Edit one line

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,3 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is coming!\n";
-print "trying another change\n";
-echo 1+1;
+print "Hello, World! This is Christopher's last change. php version is " . PHP_VERSION . "\n";
 ?>
